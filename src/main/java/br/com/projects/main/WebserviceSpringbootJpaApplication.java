633615523e4f09a0a14ddf454cd287a7dd485d6e
Heber Ribeiro <heber.r@outlook.com>
2019-11-22 15:08:06 -0300

    Projeto criado

---
+++ src/main/java/br/com/projects/main/WebserviceSpringbootJpaApplication.java
@@ -0,0 +1,13 @@
+package br.com.projects.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceSpringbootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceSpringbootJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/projects/main/WebserviceSpringbootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projects.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceSpringbootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
